6a5065fc33de78ea00502588795311d3b2d2c03b
Hixo <hixohixo32@gmail.com>
2022-10-06 22:00:43 +0200

    Add all files

---
+++ app.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.rates');
+let selectedNumber = 0;
+
+const resetElement = (element) => {
+    const activeElement = document.querySelector('.selected');
+    activeElement ? activeElement.classList.remove('selected') : '';
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if(event.target.nodeName == "DIV") return;
+        resetElement(event.target);
+        event.target.classList.add('selected');
+        selectedNumber = event.target.textContent;
+    })
+})
+
+const changeSelectedContent = () => {
+    document.querySelector('.selected_paragraph').textContent = `You selected ${selectedNumber} out of 5`
+}
+
+document.querySelector('.btn').addEventListener('click', (event) => {
+    changeSelectedContent();
+    document.querySelector('.component').classList.add('hidden');
+    document.querySelector('.ty__state').classList.remove('hidden')
+})
