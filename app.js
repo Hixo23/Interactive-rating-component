3ce0594be9fca85aa3d1d13a18a008ed6ccb8605
Hixo <hixohixo32@gmail.com>
2022-10-07 15:04:25 +0200

    Fix after code review

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('.rates');
+const buttons = document.querySelector('.rates');
 let selectedNumber = 0;
 
 const resetElement = () => {
@@ -6,14 +6,13 @@ const resetElement = () => {
     activeElement ? activeElement.classList.remove('selected') : '';
 }
 
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        if(event.target.nodeName == "DIV") return;
+
+    buttons.addEventListener('click', (event) => {
+        if(event.target.nodeName === "DIV") return;
         resetElement();
         event.target.classList.add('selected');
         selectedNumber = event.target.textContent;
     })
-})
 
 const changeSelectedContent = () => {
     document.querySelector('.selected_paragraph').textContent = `You selected ${selectedNumber} out of 5`
